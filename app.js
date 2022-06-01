ae8f1e54494022fc1d838d2071e36fd57cd0743a
Alexey Lagun <alexey.lagun@gmail.com>
2022-06-01 13:06:30 +0300

    fix: adding changes when changing sections

--- app.js
+++ app.js
@@ -117,6 +117,8 @@ var taskCompleted=function(){
 
   //Append the task list item to the #completed-tasks
   var listItem=this.parentNode;
+  var label=listItem.querySelector('.list__input-type-label');
+  label.classList.toggle('list__input-type-label-completed');
   completedTasksHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskIncomplete);
 }
@@ -127,6 +129,8 @@ var taskIncomplete=function(){
   //When the checkbox is unchecked
   //Append the task list item to the #incompleteTasks.
   var listItem=this.parentNode;
+  var label=listItem.querySelector('.list__input-type-label');
+  label.classList.toggle('list__input-type-label-completed');
   incompleteTaskHolder.appendChild(listItem);
   bindTaskEvents(listItem,taskCompleted);
 }
